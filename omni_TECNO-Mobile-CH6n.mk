8777cc8cf88ba8303a1e8aefb187c55941ae4ad5
infinixs5lite <kelvinchinedu97@gmail.com>
2022-10-15 19:17:34 +0100

    Update omni_TECNO-Mobile-CH6n.mk

--- omni_TECNO-Mobile-CH6n.mk
+++ omni_TECNO-Mobile-CH6n.mk
@@ -7,23 +7,17 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from TECNO-Mobile-CH6n device
-$(call inherit-product, device/tecno/TECNO-Mobile-CH6n/device.mk)
+# Inherit from CH6n device
+$(call inherit-product, device/tecno/CH6n/device.mk)
 
-PRODUCT_DEVICE := TECNO-Mobile-CH6n
-PRODUCT_NAME := omni_TECNO-Mobile-CH6n
-PRODUCT_BRAND := TECNO-Mobile
-PRODUCT_MODEL := TECNO Mobile CH6n
+PRODUCT_DEVICE := CH6n
+PRODUCT_NAME := twrp_CH6n
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO cammon 18
 PRODUCT_MANUFACTURER := tecno
-
-PRODUCT_GMS_CLIENTID_BASE := android-tecno
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_ch6n_h6912-user 11 RP1A.200720.011 49596 release-keys"
-
-BUILD_FINGERPRINT := TECNO-Mobile/CH6n-EU/TECNO-Mobile-CH6n:11/RP1A.200720.011/220810V72:user/release-keys
